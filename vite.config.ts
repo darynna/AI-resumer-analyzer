c0c6b8b9023fa3f4520d16eccb50d4d4c8b58e8e
Daryna Holikova <dgolikova0@gmail.com>
2026-08-19 21:11:01 +0100

    Deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/AI-resumer-analyzer/" : "/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
-});
+}));
